1cecb78711fe325745f3b1954adc631835a2f62a
andreszapataq <andreszapataq@gmail.com>
2024-09-26 18:13:23 -0500

    add dialog to show inventory-table

--- components/reference-management.tsx
+++ components/reference-management.tsx
@@ -3,7 +3,14 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import CreateReferenceModal from "./create-reference-modal";
-import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import InventoryTable from "./inventory-table";
 
 interface ReferenceData {
   code: string;
@@ -34,9 +41,19 @@ export function ReferenceManagementComponent() {
       <Button onClick={handleOpenModal}>Crear referencia</Button>
       {showLotesLink && (
         <div className="mt-4">
-          <Link href="/listado-lotes" className="text-blue-600 hover:underline">
-            Ver listado de lotes
-          </Link>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="link" className="text-blue-600 hover:underline">
+                Ver listado de lotes
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-4xl max-h-[80vh] overflow-y-auto">
+              <DialogHeader>
+                <DialogTitle>Listado de Lotes</DialogTitle>
+              </DialogHeader>
+              <InventoryTable />
+            </DialogContent>
+          </Dialog>
         </div>
       )}
       <CreateReferenceModal
